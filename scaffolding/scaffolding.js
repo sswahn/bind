95ba0994b007c7ce3b1b1ee098dadbfa30e398db
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-11 17:18:34 -0400

    Update scaffolding.js

--- scaffolding/scaffolding.js
+++ scaffolding/scaffolding.js
@@ -1,1 +1,14 @@
-//--
+const fs = require('fs-extra')
+const path = require('path')
+
+const sourceDir = __dirname  // Current directory (where your package is)
+const targetDir = process.cwd()  // The user's project directory
+
+// List of directories and files to copy
+const itemsToCopy = ['public', 'src', 'index.js']
+
+itemsToCopy.forEach(item => {
+  fs.copySync(path.join(sourceDir, item), path.join(targetDir, item))
+})
+
+console.log('Devour framework set up complete!')
