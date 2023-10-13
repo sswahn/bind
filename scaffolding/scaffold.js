5f2f311b7828b4e103e9e56453ef3395d7f87d28
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-13 15:24:30 -0400

    Update scaffold.js

--- scaffolding/scaffold.js
+++ scaffolding/scaffold.js
@@ -1,28 +1,21 @@
-const fs = require('fs-extra');
-const path = require('path');
-const { execSync } = require('child_process');
+const fs = require('fs-extra')
+const path = require('path')
 
-const scriptDirectory = __dirname;
-const projectDirectory = process.cwd();
+const scriptDirectory = __dirname
+const projectDirectory = process.cwd()
 
-const sourceDir = path.join(scriptDirectory, 'templates', 'base'); 
-const targetDir = projectDirectory;
+const sourceDir = path.join(scriptDirectory, 'templates', 'base')
+const targetDir = projectDirectory
 
 // Copy template files to the target directory
-fs.copySync(sourceDir, targetDir);
+fs.copySync(sourceDir, targetDir)
 
 // Rename package.json.template to package.json
-const packageTemplatePath = path.join(targetDir, 'package.json.template');
-const packagePath = path.join(targetDir, 'package.json');
+const packageTemplatePath = path.join(targetDir, 'package.json.template')
+const packagePath = path.join(targetDir, 'package.json')
 
 if (fs.existsSync(packageTemplatePath)) {
-    fs.renameSync(packageTemplatePath, packagePath);
+    fs.renameSync(packageTemplatePath, packagePath)
 }
 
-// Install dependencies
-execSync('npm install', {
-  cwd: targetDir,
-  stdio: 'inherit'
-});
-
-console.log('Project scaffolded and dependencies installed successfully!');
+console.log('Project scaffold successful.')
