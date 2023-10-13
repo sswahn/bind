368b1389e369675619a44585338f11cbf6fb7564
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-13 11:45:14 -0400

    Update dom.test.js

--- test/dom.test.js
+++ test/dom.test.js
@@ -1,1 +1,139 @@
-//--
+import { html, render } from './path_to_functions_file'; // replace 'path_to_functions_file' with the path to your functions file
+
+describe('html function', () => {
+  
+  test('it should create a valid HTML element', () => {
+    const element = html('div', { id: 'test' }, ['Hello']);
+    expect(element.tagName).toBe('DIV');
+    expect(element.id).toBe('test');
+    expect(element.textContent).toBe('Hello');
+  });
+
+  test('it should handle events correctly', () => {
+    let clicked = false;
+    const handleClick = () => clicked = true;
+
+    const button = html('button', { onClick: handleClick }, ['Click Me']);
+    button.click();
+
+    expect(clicked).toBe(true);
+  });
+
+  test('it should handle textContent attribute', () => {
+    const element = html('span', { textContent: 'Hello, World!' }, []);
+    expect(element.textContent).toBe('Hello, World!');
+  });
+
+  test('it should handle child nodes', () => {
+    const child = document.createElement('span');
+    child.textContent = 'Child';
+
+    const element = html('div', {}, ['Hello', child]);
+
+    expect(element.childNodes.length).toBe(2);
+    expect(element.childNodes[1].textContent).toBe('Child');
+  });
+
+    test('it should error if first argument is not a string', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    html(123, {}, []);
+    
+    expect(consoleSpy).toHaveBeenCalledWith('TypeError: html first argument must be a string.');
+    
+    consoleSpy.mockRestore();
+  });
+
+  test('it should error if second argument is not an object literal', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    html('div', [], []);
+    
+    expect(consoleSpy).toHaveBeenCalledWith('TypeError: html second argument must be an object literal.');
+    
+    consoleSpy.mockRestore();
+  });
+
+  test('it should error if third argument is not an array', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    html('div', {}, 'Hello');
+    
+    expect(consoleSpy).toHaveBeenCalledWith('TypeError: html third argument must be an array.');
+    
+    consoleSpy.mockRestore();
+  });
+
+  test('it should set attributes and properties correctly', () => {
+    const dummyFunc = () => {};
+    const element = html('div', { class: 'test', onclick: dummyFunc }, []);
+
+    expect(element.className).toBe('test'); // This is a property
+    expect(element.onclick).toBe(dummyFunc); // This is also a property
+    expect(element.getAttribute('class')).toBe('test'); // This is an attribute
+  });
+
+  test('it should convert non-Node children to text nodes', () => {
+    const element = html('div', {}, [123, true]);
+
+    expect(element.childNodes.length).toBe(2);
+    expect(element.childNodes[0].nodeType).toBe(Node.TEXT_NODE);
+    expect(element.childNodes[0].textContent).toBe('123');
+    expect(element.childNodes[1].nodeType).toBe(Node.TEXT_NODE);
+    expect(element.childNodes[1].textContent).toBe('true');
+  });
+});
+
+describe('render function', () => {
+  
+  const root = document.createElement('div');
+
+  test('it should append an element to root', () => {
+    const element = html('div', {}, ['Hello']);
+    render(element, root);
+
+    expect(root.firstChild).toBe(element);
+  });
+
+  test('it should replace existing component', () => {
+    const element1 = html('div', {}, ['First']);
+    render(element1, root);
+    
+    const element2 = html('div', {}, ['Second']);
+    render(element2, root);
+    
+    expect(root.firstChild).toBe(element2);
+    expect(root.children.length).toBe(1);
+  });
+
+    test('it should error if element is not an instance of Element', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    render('string', document.createElement('div'));
+
+    expect(consoleSpy).toHaveBeenCalledWith('Invalid component element provided to render.');
+    
+    consoleSpy.mockRestore();
+  });
+
+  test('it should error if root is not an instance of Element', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    render(document.createElement('div'), 'string');
+
+    expect(consoleSpy).toHaveBeenCalledWith('Invalid root element provided to render.');
+    
+    consoleSpy.mockRestore();
+  });
+
+  test('it should replace the element if rendered multiple times', () => {
+    const element = html('div', {}, ['Hello']);
+    render(element, root); 
+    const originalChildCount = root.children.length;
+
+    render(element, root); // Rendering again
+
+    expect(root.children.length).toBe(originalChildCount); // Ensure we didn't append it again.
+  });
+});
+
