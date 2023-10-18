059de6e1aabacc90acb8929c359a0b5d1a6b3891
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-18 09:28:21 -0400

    Update bind.js

--- src/core/bind.js
+++ src/core/bind.js
@@ -7,8 +7,6 @@ const observables = new WeakMap()
 // TODO: unit tests
 
 export const createStore = initialState => {
-  console.log('createStore: ', initialState)
-  
   if (typeof initialState !== 'object' || Array.isArray(initialState)) {
     return console.error('TypeError: createStore argument must be an object literal.')
   }
@@ -16,8 +14,6 @@ export const createStore = initialState => {
 }
   
 const dispatch = action => {
-  console.log('dispatch: ', action)
-  
   if (typeof action !== 'object' || Array.isArray(action)) {
     return console.error('TypeError: dispatch argument must be an object literal.')
   }
@@ -30,26 +26,16 @@ const dispatch = action => {
   if (!Object.keys(state).includes(action.type)) {
     return console.error(`Dispatched action type ${action.type} is not found in current state.`)
   }
-
-  try {
-  
   const key = queue.size + 1
   queue.set(key, action)
-
   if (queue.size === 1) {
     queueMicrotask(() => {
       processQueue(key)
     })
   }
-
-  } catch (error) {
-    console.error(`Error dispatching action: ${error}`)
-  }
 }
 
 const processQueue = key => {
-  console.log('processQueue: ', key)
-  
   try {
     const {type, payload} = queue.get(key)
     updateState(type, payload)
@@ -62,23 +48,14 @@ const processQueue = key => {
 }
 
 const updateState = (type, payload) => {
-  console.log('updateState: ', {type, payload})
-
-  try {
-    
   if (typeof payload === 'function') {
     state = structuredClone({...state, [type]: payload(state[type])})
   } else {
     state = structuredClone({...state, [type]: payload})
   }
-    
-  } catch (error) {
-    console.error(`Error updating state: ${error}`)
-  }
 }
 
 const notifySubscribers = type => {
-  console.log('notifySubscribers: ', type)
   try {
     const array = subscribers.get(type)
     array?.forEach(item => handleNotification(item, type))
@@ -88,8 +65,6 @@ const notifySubscribers = type => {
 }
 
 const handleNotification = (item, type) => {
-  console.log('handleNotification: ', {item, type})
-  
   try {
     const { component, parameters } = item
     const liveNode = components.get(component)
@@ -102,8 +77,6 @@ const handleNotification = (item, type) => {
 }
 
 const continueProcessingQueue = key => {
-  console.log('continueProcessingQueue: ', key)
-  
   if (queue.size > 0) {
     processQueue(key + 1)
   }
@@ -134,14 +107,10 @@ const processBatch = () => {
 } */
 
 const deepClone = value => {
-  console.log('deepClone: ', value)
-  
   return typeof value === 'object' && value !== null ? structuredClone(value) : value
 }
 
 export const bind = (type, component) => {
-  console.log('bind: ', {type, component})
-  
   if (typeof type !== 'string') {
     return console.error('TypeError: bind function first argument must be a string.')
   }
@@ -159,8 +128,6 @@ export const bind = (type, component) => {
 }
 
 const unbind = (type, component) => {
-  console.log('unbind: ', {type, component})
-  
   try {
     const subscription = subscribers.get(type)
     const filtered = subscription.filter(item => item.component !== component)
@@ -171,13 +138,11 @@ const unbind = (type, component) => {
 }
 
 const observe = (element, type, component) => {
-  console.log('observe: ', {element, type, component})
-  
   if (!(element instanceof Element)) {
     return console.error('Bound components must return instances of Element.')
   }
   if (observables.get(element)) {
-    return console.warn('Element already being observed.')
+    return console.warn(`Element already being observed: ${element}`)
   }
   observables.set(element, true)
   const observer = new MutationObserver(mutations => {
@@ -186,13 +151,10 @@ const observe = (element, type, component) => {
     }, [])
     for (let node of removed) {
       if (node === element) {
-
-        console.log('observed removal of: ', node)
-        
         unbind(type, component)
         observables.delete(element)
         observer.disconnect()
-        return console.log('Element unbound, unobserved.')
+        return console.log(`Node unbound, unobserved: ${node}`)
       }
     }
   })
