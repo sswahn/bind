513726407fef973c178ebb8d52f8c57f97e14e9f
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-21 19:06:23 -0400

    Update dom.js

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,4 +1,5 @@
 const handlersRegistry = new Map()
+const nonBubblingEvents = ['change', 'error', 'load', 'mouseenter', 'mouseleave', 'reset', 'scroll', 'unload']
 
 const delegateEventHandling = event => {
   const target = event.target
@@ -14,32 +15,41 @@ const delegateEventHandling = event => {
 
 export const html = (type, attributes = {}, children = []) => {
   if (typeof type !== 'string') {
-    return console.error('TypeError: html first argument must be a string.')
+    throw new TypeError('html: Expected first argument to be a string for element type.')
   }
   if (typeof attributes !== 'object' || Array.isArray(attributes)) {
-    return console.error('TypeError: html second argument must be an object literal.')
+    throw new TypeError('html: Expected second argument to be an object literal for attributes.')
   }
   if (!Array.isArray(children)) {
-    return console.error('TypeError: html third argument must be an array.')
+    throw new TypeError('html: Expected third argument to be an array for children elements.')
   }
   const element = document.createElement(type)
   Object.entries(attributes).forEach(([key, value]) => {
     if (key.startsWith('on') && typeof value === 'function') {
       const eventType = key.slice(2).toLowerCase()
+      const useCapture = nonBubblingEvents.includes(eventType)
       if (!handlersRegistry.has(eventType)) {
         handlersRegistry.set(eventType, new Map())
-        document.body.addEventListener(eventType, delegateEventHandling)
+        document.body.addEventListener(eventType, delegateEventHandling, useCapture)
       }
       handlersRegistry.get(eventType).set(element, value)
     } else if (key === 'textContent') {
       element.textContent = value
+    } else if (typeof value !== 'string') {
+      throw new TypeError(`html: Expected attribute value to be a string for attribute ${key}. Received type ${typeof value}.`)
     } else {
       element.setAttribute(key, value)
     }
   })
-  const nodes = children.map(child => 
-    child instanceof Node ? child : document.createTextNode(child)
-  )
+  const nodes = children.map(child => {
+    if (child instanceof Node) {
+      return child
+    } else if (typeof child === 'string') {
+      return document.createTextNode(child)
+    } else {
+      throw new TypeError('html: Expected child elements to be of type Node or string.')
+    }
+  })
   element.append(...nodes)
   return element
 }
